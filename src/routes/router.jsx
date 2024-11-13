447ac0e85f0083676ec5e07e9cb39fbe9433400b
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2024-11-14 00:45:15 +0600

    project complete header complete breaking news marquee added

---
+++ src/routes/router.jsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomeLayout from "../layouts/HomeLayout";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomeLayout></HomeLayout>
+    },
+    {
+        path: '/news',
+        element: <h1>news layout</h1>
+
+    },
+    {
+        path: 'auth',
+        element: <h1>login</h1>
+    },
+    {
+        path: '*',
+        element: <h1>error</h1>
+    }
+
+]);
+export default router;
